ff8df96282274366c9d4dc88a5946e3965e94850
TeachMePLZ <sfgraff@yandex.ru>
2023-03-07 21:07:22 +0300

    emptty project

---
+++ eureka-client/src/test/java/ru/friendmicro/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.friendmicro.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eureka-client2/src/test/java/ru/friendmicro/eurekaclient2/EurekaClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.friendmicro.eurekaclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
